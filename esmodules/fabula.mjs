aba477a04ade5275a8ad6bce27b1d5772abd713e
kleingulo <malte.bruhn@arcor.de>
2025-03-04 18:29:53 +0100

    error correction

--- esmodules/fabula.mjs
+++ esmodules/fabula.mjs
@@ -1,11 +1,27 @@
-function updateZeroPower(actor){
-	var zero = actor.items.filter((item) => item.system.fuid == "ZeroPower")[0]
-	var value = zero.system.data.progress.current
-	var max = zero.system.data.progress.max
-	//actor.system.resources.zero = {"value":value, "max":max};
-	actor.setFlag('FabulaHelper', 'zero', {"value":value, "max":max});
-};
+class FabulaHelper{
+	static ID = 'GuloFabulaHelper';
+	
+	static updateZeroPower(actor){
+		var zero = actor.items.filter((item) => item.system.optionalType == "projectfu.zeroPower")[0]
+		if(!!zero)
+		{
+			var value = zero.system.data.progress.current
+			var max = zero.system.data.progress.max
+			//actor.system.resources.zero = {"value":value, "max":max};
+			actor.setFlag(FabulaHelper.ID, 'zeroPower', {"value":value, "max":max});
+		}
+		else{
+			actor.setFlag(FabulaHelper.ID, 'zeroPower', {"value":0, "max":0});
+		}
+		
+	};
 
-//CONFIG.Actor.trackableAttributes.character.bar.push("resources.zero");
-CONFIG.Actor.trackableAttributes.character.bar.push("parent.flags.FabulaHelper.zero");
-Hooks.on('projectfu.actor.dataPrepared', (actor) => updateZeroPower(actor));
+	static initialize()
+	{
+		CONFIG.Actor.trackableAttributes.character.bar.push("parent.flags."+FabulaHelper.ID+".zeroPower");
+	}
+
+}
+
+Hooks.on('init', FabulaHelper.initialize);
+Hooks.on('projectfu.actor.dataPrepared', (actor) => FabulaHelper.updateZeroPower(actor));
