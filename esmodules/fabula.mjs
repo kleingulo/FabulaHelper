4fcdd36de886b1341aa778daee7ed89de79aa422
kleingulo <malte.bruhn@arcor.de>
2025-08-02 21:31:01 +0200

    Added zero power macro logic

--- esmodules/fabula.mjs
+++ esmodules/fabula.mjs
@@ -19,5 +19,54 @@ class FabulaHelper{
 
 }
 
+function processCheckHook(check, actor, item)
+{
+  // Trigger the item roll
+	if (item.hasMacro())
+	{
+		fireMacro(item.parent, item);
+	}
+}
+
+function chatMessageHook(chatMessage, speaker, mod)
+{
+	if('projectfu' in chatMessage.flags)
+	{
+		if('Item' in chatMessage.flags.projectfu)
+		{
+			var item = chatMessage.flags.projectfu.Item;
+			if(item.type == "optionalFeature")
+			{
+				if (item.hasMacro())
+				{
+					fireMacro(item.parent, item);
+				}
+			}
+		}
+	}
+}
+
+function fireMacro(actor, item)
+{
+	var protoname = actor.prototypeToken.name;
+	var src = canvas.tokens.ownedTokens.find((token) => token.name == protoname);
+	var count = 0;
+	if(item.system.hasOwnProperty("targeting"))
+	{
+		if(item.system.targeting.rule == "self")
+			{
+			item.executeMacro({"src":src, "tgt":null, "cnt":0});
+			}
+	}
+	
+	game.user.targets.ids.forEach((targetid) => {
+	var tgt = canvas.tokens.get(targetid);
+	item.executeMacro({"src":src, "tgt":tgt, "cnt":count});
+	count = count+1;
+	});
+}
+
 Hooks.on('init', FabulaHelper.initialize);
 Hooks.on('preUpdateItem', (item, patch, modified) => FabulaHelper.updateZeroPower(item, patch));
+Hooks.on("projectfu.processCheck", processCheckHook);
+Hooks.on("preCreateChatMessage", chatMessageHook);
